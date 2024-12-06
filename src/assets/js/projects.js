51370181b7e74a3cda37f462958c6d4f96426b5b
krana <kranat10@gmail.com>
2024-12-05 22:18:29 -0600

    Adding basic structure of portfolio, blog page maps over psudo project data

---
+++ src/assets/js/projects.js
@@ -0,0 +1,21 @@
+// import img1 from 'path-to-img'
+// import img2 from 'path-to-img'
+
+
+const projectsData = [
+
+    {
+        name: "project-1",
+        description: "descripton",
+        image: 'img1',
+        path: "github.com"
+    },
+    {
+        name: "project-2",
+        description: "descripton",
+        image: 'img2',
+        path: "github.com"
+    },
+]
+
+export default projectsData;
---
+++ src/components/pages/Blog.jsx
@@ -0,0 +1,21 @@
+import projectsData from "../../assets/js/projects";
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <div className="cards-container">
+        {projectsData.map((project, index) => (
+          <div className="card" key={index}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <img src={project.image} alt={project.name} />
+            <a href={project.path} target="_blank" rel="noopener noreferrer">
+              <button>View More</button>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
