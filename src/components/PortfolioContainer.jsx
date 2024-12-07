82ab2b866de6beb639d014da7933712158e964d1
krana <kranat10@gmail.com>
2024-12-06 20:10:57 -0600

    added Contact.css and updated Contact.jsx

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -3,7 +3,7 @@ import NavTabs from './NavTabs';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import Contact from './pages/Contact/Contact';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
