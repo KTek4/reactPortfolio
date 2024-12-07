fadb50b560f5a501dab959f8d0396a2982fa730b
krana <kranat10@gmail.com>
2024-12-06 20:25:42 -0600

    added images and relevant file paths

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,21 +1,48 @@
-// import img1 from 'path-to-img'
-// import img2 from 'path-to-img'
+import img1 from 'new-portfolio/src/assets/imgs/awkward.jpg'
+import img2 from 'new-portfolio/src/assets/imgs/book.jpg'
+import img3 from 'new-portfolio/src/assets/imgs/dragon.jpg'
+import img4 from 'new-portfolio/src/assets/imgs/griffin.jpg'
+import img5 from 'new-portfolio/src/assets/imgs/pegasus.jpg'
+import img6 from 'new-portfolio/src/assets/imgs/werewolf.jpg'
 
+const projectsData = [
 
-// const projectsData = [
+    {
+        name: "project-1",
+        description: "descripton",
+        image: img1,
+        path: "github.com"
+    },
+    {
+        name: "project-2",
+        description: "descripton",
+        image: img2,
+        path: "github.com"
+    },
+    {
+        name: "project-3",
+        description: "descripton",
+        image: img3,
+        path: "github.com"
+    },
+    {
+        name: "project-4",
+        description: "descripton",
+        image: img4,
+        path: "github.com"
+    },
+    {
+        name: "project-5",
+        description: "descripton",
+        image: img5,
+        path: "github.com"
+    },
+    {
+        name: "project-6",
+        description: "descripton",
+        image: img6,
+        path: "github.com"
+    },
+]
 
-//     {
-//         name: "project-1",
-//         description: "descripton",
-//         image: 'img1',
-//         path: "github.com"
-//     },
-//     {
-//         name: "project-2",
-//         description: "descripton",
-//         image: 'img2',
-//         path: "github.com"
-//     },
-// ]
-
-// export default projectsData;
+export default projectsData;
