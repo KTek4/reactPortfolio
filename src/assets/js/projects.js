a818a16827144d96a0e122456652eceb1412a152
krana <kranat10@gmail.com>
2024-12-06 20:14:41 -0600

    temporarily disabled project.js so that web page can properly function

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -2,20 +2,20 @@
 // import img2 from 'path-to-img'
 
 
-const projectsData = [
+// const projectsData = [
 
-    {
-        name: "project-1",
-        description: "descripton",
-        image: 'img1',
-        path: "github.com"
-    },
-    {
-        name: "project-2",
-        description: "descripton",
-        image: 'img2',
-        path: "github.com"
-    },
-]
+//     {
+//         name: "project-1",
+//         description: "descripton",
+//         image: 'img1',
+//         path: "github.com"
+//     },
+//     {
+//         name: "project-2",
+//         description: "descripton",
+//         image: 'img2',
+//         path: "github.com"
+//     },
+// ]
 
-export default projectsData;
+// export default projectsData;
